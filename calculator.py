7df61f939513a72fbff8cb756e469329cd1aafb9
William Akingba <X00179468@mytudublin.ie>
2024-12-20 13:51:33 +0000

    Added comments to my code

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
+"""A simple python calculator module for basic math functions ."""
+
 def add(a, b):
+    """Returns the sum of a and b."""
     return a + b
 
 def subtract(a, b):
+    """Returns the difference between a and b."""
     return a - b
 
-
-
 def multiply(a, b):
+    """Returns the product of a and b."""
     return a * b
 
 def divide(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Error: Division by zero"
+    """Returns the quotient of a and b."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
